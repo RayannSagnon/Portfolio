36f0ef4a75ce7ba47059f53b3aa2c6cdb039a6ca
Rayann Sagnon <rayannsagnon60gmail.com>
2026-01-22 16:51:38 -0500

    Upgrades to images position

--- src/App.tsx
+++ src/App.tsx
@@ -26,47 +26,48 @@ function App() {
         </div>
       </header>
 
-      {/* Hero Hi There */}
-      <section className="hero-hi-there">
-        <h1 className="hero-hi">Hi</h1>
-        <div className="hero-image-center">
-          <img src={eloquence} alt="Portrait" />
-        </div>
-        <h1 className="hero-there">There</h1>
-      </section>
-
-      {/* Section I AM RAYANN */}
-      <section className="section-intro">
-        <div className="intro-left">
-          <h2 className="intro-am">I AM</h2>
-          <h2 className="intro-rayann">RAYANN</h2>
-          <p className="intro-description">
-            I am a Computer Engineering student passionate about building impactful digital solutions, blending software, AI, and design.
-          </p>
-        </div>
-
-        {/* Collage Grid */}
-        <div className="collage-wrapper">
-          <div className="collage-grid">
-            <img src={parents} alt="img1" className="col-item col-1" />
-            <img src={montreal} alt="img2" className="col-item col-2" />
-            <img src={falls} alt="img3" className="col-item col-3" />
-            <img src={brevet} alt="img4" className="col-item col-4" />
-            <img src={diplome} alt="img5" className="col-item col-5" />
-            <img src={team} alt="img6" className="col-item col-6" />
-            <img src={kart} alt="img7" className="col-item col-7" />
+      {/* Hero Section - All Images and Text */}
+      <section className="hero">
+        <div className="hero-top">
+          <h1 className="hero-hi">Hi</h1>
+          <div className="hero-image-center">
+            <img src={eloquence} alt="Portrait" />
           </div>
+          <h1 className="hero-there">There</h1>
         </div>
 
-        {/* Right Text */}
-        <div className="intro-right">
-          <p className="specialization">
-            Specialized in Front-End Development, UI/UX Design, and Web Applications.
-          </p>
-          <h3 className="title-engineer">
-            <span>COMPUTER</span>
-            <span>ENGINEER</span>
-          </h3>
+        <div className="hero-middle">
+          <div className="hero-left">
+            <h2 className="intro-am">I AM</h2>
+            <h2 className="intro-rayann">RAYANN</h2>
+            <p className="intro-description">
+              I am a Computer Engineering student passionate about building impactful digital solutions, blending software, AI, and design.
+            </p>
+          </div>
+
+          {/* Collage Grid */}
+          <div className="collage-wrapper">
+            <div className="collage-grid">
+              <img src={parents} alt="img1" className="col-item col-1" />
+              <img src={montreal} alt="img2" className="col-item col-2" />
+              <img src={falls} alt="img3" className="col-item col-3" />
+              <img src={brevet} alt="img4" className="col-item col-4" />
+              <img src={diplome} alt="img5" className="col-item col-5" />
+              <img src={team} alt="img6" className="col-item col-6" />
+              <img src={kart} alt="img7" className="col-item col-7" />
+            </div>
+          </div>
+
+          {/* Right Text */}
+          <div className="hero-right">
+            <p className="specialization">
+              Specialized in Front-End Development, UI/UX Design, and Web Applications.
+            </p>
+            <h3 className="title-engineer">
+              <span>COMPUTER</span>
+              <span>ENGINEER</span>
+            </h3>
+          </div>
         </div>
       </section>
 
